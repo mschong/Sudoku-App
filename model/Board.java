46d748d4249f4933bfbb880abee0ec97a9d8a097
Marina Chong <marinachong96@gmail.com>
2018-02-19 21:12:17 -0700

    Update Board.java

--- model/Board.java
+++ model/Board.java
@@ -11,6 +11,7 @@ public class Board {
 
     public int[][] grid;
     public boolean win;
+    public boolean inRow, inCol, inSquare;
 
     /** Create a new board of the given size. */
     public Board(int size) {
@@ -27,7 +28,7 @@ public class Board {
         grid = new int[size][size];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
-                grid[i][j] = -1;
+                grid[i][j] = 0;
             }
         }
 
@@ -35,48 +36,53 @@ public class Board {
 
     public void insertNumber(int x, int y, int n) {
         // while game is not over
-        while (!win) {
+        //while (!win) {
             // check if valid number
             if (!checkNum(x, y, n)) {
                 System.out.println("Not a valid number");
-                continue;
+                //continue;
             }
             System.out.println("Valid number, inserting " + n);
             checkWin();
-        }
+     //   }
+        printGrid();
+
     }
 
     private boolean checkNum(int x, int y, int n) {
         // check if is outside grid or if is not between 1 and 9
-        if (n < 1 || n > 9 || x < 0 || y < 0 || x > 8 || y > 8) {
+        if (n > 9 || x < 0 || y < 0 || x > 8 || y > 8) {
             return false;
         }
         // check if current space is not part of the pre-filled values
-        if(grid[x][y] > 0) {
+        /**if(grid[y][x] > 0) {
             System.out.println("Already filled with pre-values");
             return false;
-        }
+        }**/
 
         // check column
         if (!checkColumn(x, n)) {
             System.out.println("Number already in column");
+            inCol = true;
             return false;
         }
 
         // check row
         if (!checkRow(y, n)) {
             System.out.println("Number already in row");
+            inRow = true;
             return false;
         }
 
         // check square
-        if (!checkSquare(x, y, n)) {
+        if (!checkSquare(y, x, n)) {
             System.out.println("Number already in square");
+            inSquare = true;
             return false;
         }
 
         // if number passes every test add to grid
-        grid[x][y] = n;
+        grid[y][x] = n;
         return true;
     }
 
@@ -96,7 +102,7 @@ public class Board {
 
     private boolean checkRow(int y, int n) {
         for (int row = 0; row < grid.length; row++) {
-            if (grid[row][y] == n) {
+            if (grid[y][row] == n) {
                 return false;
             }
         }
@@ -105,7 +111,7 @@ public class Board {
 
     private boolean checkColumn(int x, int n) {
         for (int col = 0; col < grid.length; col++) {
-            if (grid[x][col] == n) {
+            if (grid[col][x] == n) {
                 return false;
             }
         }
@@ -118,7 +124,7 @@ public class Board {
             for (int j = 0; j < grid.length; j++) {
                 sum += grid[i][j];
                 // if there are still empty squares
-                if (grid[i][j] == -1) {
+                if (grid[i][j] == 0) {
                     return;
                 }
             }
@@ -150,10 +156,33 @@ public class Board {
     }
 
 
+    public void printGrid() {
+        System.out.println("\n+===+===+===+===+===+===+===+===+===+");
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < grid.length; j++) {
+                if (j % 3 == 2) {
+                    System.out.print(" " + ((grid[j][i]==0) ? " ":grid[j][i]) + " !");
+                } else {
+                    System.out.print(" " + ((grid[j][i]==0) ? " ":grid[j][i]) + " |");
+                }
+            }
+            if (i % 3 == 2) {
+                System.out.println("\n+===+===+===+===+===+===+===+===+===+");
+
+            } else {
+                System.out.println("\n+---+---+---+---+---+---+---+---+---+");
+            }
+        }
+    }
+
+    public void insertZero(int x, int y){
+        grid[y][x] = 0;
+    }
+
     /** Return the size of this board. */
     public int size() {
     	return size;
     }
 
-    // WRITE YOUR CODE HERE ..
 }
